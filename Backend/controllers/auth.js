363205fbd19cceb0965f00fcf13d1db40fbdc689
Saivardhan655 <saivardhan9154@gmail.com>
2024-09-26 18:36:12 +0530

    login completed

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -2,13 +2,36 @@ const User=require('../models/User')
 const bcrypt=require('bcryptjs')
 const {StatusCodes}=require('http-status-codes')
 const {BadRequestError,UnauthenticatedError}=require('../errors')
-const login =async (req,res)=>{
-    res.status(200).send('login called');
-}
+
 const register=async (req,res)=>{
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: 'Name, email, and password are required.' });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).json({ message: 'Email already in use.' });
+    }
     const user=await User.create({ ...req.body })
-    //const token=user.createJWT()
-    res.status(StatusCodes.CREATED).json({ user :{name:user.name}})
+    const token=user.createJWT()
+    res.status(StatusCodes.CREATED).json({ user :{name:user.name},token})
+}
+
+const login=async (req,res)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user=await User.findOne({email})
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const isPasswordCorrect=await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+    const token=user.createJWT()
+    res.status(StatusCodes.OK).json({user:{name:user.name},token})
 }
 
 module.exports={
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,6 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 const UserSchema=new mongoose.Schema({
     name:{
@@ -12,7 +14,6 @@ const UserSchema=new mongoose.Schema({
         match:[/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,'please provide eamil'],
         required:true,
         lowercase:true,
-        dropDups: true,
         unique:true,
     },
     password:{
@@ -21,5 +22,18 @@ const UserSchema=new mongoose.Schema({
         require:[true,'please enter password'],
     }
 })
+UserSchema.pre('save',async function(next){
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt)
+    next()
+})
 
+UserSchema.methods.createJWT=function(){
+    return jwt.sign({userId:this._id,name:this.name},process.env.JWT_SECRET,{expiresIn: process.env.JWT_LIFETIME})
+}
+
+UserSchema.methods.comparePassword=async function(candidatePassword){
+    const isMatch=await bcrypt.compare(candidatePassword,this.password)
+    return isMatch
+}
 module.exports=mongoose.model('User',UserSchema)
